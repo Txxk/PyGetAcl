1e8a07704742707b65d5eb759a64fa62572e395b
Txxk <393289038@qq.com>
2014-10-21 15:56:24 +0800

    修改start_showtable.sh,加入变量

--- start_showtable.sh
+++ start_showtable.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cat per_file.txt|while read per
+
+#real_path
+real_path=`cd $(dirname $0) && pwd`
+cd ${real_path}
+
+cat ${real_path}/per_file.txt|while read per
 do
-    #echo "/tmp/${per%%_*}_permission.txt"
-    /usr/bin/python P_table.py ${per} > /tmp/${per%%_*}_permission.txt
+    /usr/bin/python ${real_path}/P_table.py ${per} > ${real_path}/${per%%_*}_permission.txt
 done
